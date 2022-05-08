3cb6856b1be2aa04e3119cf92809e607d668eb39
hsoffos <huntersoffos@gmail.com>
2022-05-08 01:09:25 -0400

    Bollinger Bands!

---
+++ BollingerBands.py
@@ -0,0 +1,23 @@
+import pandas as pd
+import requests
+import matplotlib.pyplot as plt
+
+func = 'BBANDS'
+symbol = 'IBM'
+interval = 'weekly'
+time_period = '60'
+series_type = 'close'
+api = 'KM2RBMGCOTIXETTD'
+datatype = 'csv'
+
+df = pd.read_csv('https://www.alphavantage.co/query?function={}&symbol={}&' \
+                 'interval={}&time_period={}&series_type={}&apikey={}&' \
+                 'datatype={}'.format(func, symbol, interval, time_period, series_type, api, datatype),
+                 index_col='time')
+
+df = df.iloc[:-1000, :]
+df = df.iloc[::-1]
+print(df)
+
+df.plot()
+plt.show()
